cbf851cafd84ea72cb05f1b1023fe5eff5e15338
Sourabh Bhagat <sbhagat@aerospike.com>
2021-12-03 17:52:59 -0800

    add cert-manager yaml

---
+++ fleet.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script to install fleet
+
+# install helm
+brew install helm
+
+# get latest fleet
+helm -n fleet-system install --create-namespace --wait fleet-crd https://github.com/rancher/fleet/releases/download/v0.3.3/fleet-crd-0.3.3.tgz
+helm -n fleet-system install --create-namespace --wait fleet https://github.com/rancher/fleet/releases/download/v0.3.3/fleet-0.3.3.tgz
