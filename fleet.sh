6ead1e9f4df8b81206ccca91386ef26a9b6397b1
Sourabh Bhagat <sbhagat@aerospike.com>
2021-12-09 13:58:36 -0800

    upgrade fleet, add ako

--- fleet.sh
+++ fleet.sh
@@ -6,5 +6,5 @@
 brew install helm
 
 # get latest fleet
-helm -n fleet-system install --create-namespace --wait fleet-crd https://github.com/rancher/fleet/releases/download/v0.3.3/fleet-crd-0.3.3.tgz
-helm -n fleet-system install --create-namespace --wait fleet https://github.com/rancher/fleet/releases/download/v0.3.3/fleet-0.3.3.tgz
+helm -n fleet-system install --create-namespace --wait fleet-crd https://github.com/rancher/fleet/releases/download/v0.3.7/fleet-0.3.7.tgz
+helm -n fleet-system install --create-namespace --wait fleet https://github.com/rancher/fleet/releases/download/v0.3.7/fleet-crd-0.3.7.tgz
